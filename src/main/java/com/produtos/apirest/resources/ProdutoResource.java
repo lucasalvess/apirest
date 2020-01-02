ef71913bda0c35439ba35c2af4e3ea02dcb8f65b
Lucas Alves <lcluphs@gmail.com>
2020-01-02 18:43:01 -0300

    add findby id route and method in repository

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,7 +3,12 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,10 @@ public class ProdutoResource {
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
+	
+	@GetMapping("/produto/{id}")
+	public Produto listaProdutoUnico(@PathVariable(value="id") long id){
+		return produtoRepository.findById(id);
+	}
+
 }
